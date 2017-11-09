fc8577766b01b0ec12a3425e279f9c8576e58cd9
Abhay Bhosale <kudosabhay@gmail.com>
2017-11-09 18:22:52 +0530

    first shell script

---
+++ hello.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+####################################
+#
+# First Script for JenkinsRepo.
+# Let's print hello world	
+#
+####################################
+CURL='/usr/bin/curl'
+
+echo "Hello from your first script"
